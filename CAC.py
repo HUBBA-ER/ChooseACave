2466b59fe3a78551ca346020bcd179a4ee221935
Tripp Lyons <tripplyons@gmail.com>
2015-09-28 16:54:27 -0700

    Logic Fix

--- CAC.py
+++ CAC.py
@@ -3,8 +3,11 @@ print("Welcome to.... Choose A Cave!! (Or you die)")
 name = input("Enter a name for the leaderboard, please:")
 print("Thank you. To play the game, type 'play' to learn how to play, type 'info'")
 gamecmd = input("Type 'play' or 'info':)
-if gamecmd == "play":
-  #GAME HERE
-if gamecmd == "info":
-  print("Just type in a random guessed number between 1 and 8 and hope you get it right... that's all there is to it.")
-  
+while(gamecmd != "play"):
+  if gamecmd == "info":
+    print("Just type in a random guessed number between 1 and 8 and hope you get it right... that's all there is to it.")
+    gamecmd = input("Type 'play' or 'info':)
+
+####################
+## GAME GOES HERE ##
+####################
