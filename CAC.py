61b68298833bc95a1d1e5802511f9c74479fef0c
KabirBhatia <kabirbhatia04@gmail.com>
2015-09-29 16:11:43 -0700

    Update CAC.py
    initial semantics update. issues created and fixed (SORRY, TRIPP)

--- CAC.py
+++ CAC.py
@@ -1,13 +1,23 @@
 #Choose A Cave game menu
-print("Welcome to.... Choose A Cave!! (Or you die)")
-name = input("Enter a name for the leaderboard, please:")
-print("Thank you. To play the game, type 'play' to learn how to play, type 'info'")
-gamecmd = input("Type 'play' or 'info':)
+def menu():
+  print("Welcome to.... Choose A Cave!! (Or you die)")
+  name = input("Enter a name for the leaderboard, please:")
+  print("Thank you. To play the game, type 'play' to learn how to play, type 'info' to learn how to play, and credits for credits")
+  #Runs menu
+menu()
+gamecmd = input("Type 'play' or 'info' or 'credits':")
 while(gamecmd != "play"):
   if gamecmd == "info":
     print("Just type in a random guessed number between 1 and 8 and hope you get it right... that's all there is to it.")
-  gamecmd = input("Type 'play' or 'info':)
-
-####################
-## GAME GOES HERE ##
-####################
+    doyawanna = input("Want to return to menu? yes or no:")
+    if doyawanna == "yes":
+        menu()
+    else:
+        game()
+  if gamecmd == "credits":
+    print("Created by: Kabir Bhatia, Coding by: Kabir Bhatia and Tripp Lyons, developed by: Tripp Lyons")
+    menu()
+def game():
+    print("gamegamegame")
+while(gamecmd == "play"):
+    game()
